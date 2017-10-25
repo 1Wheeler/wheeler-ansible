8fbda20cc552eb7c5e5ba04df5b0cce34c41ff91
1Wheeler <jwheeler@morpheusdata.com>
2017-10-25 15:47:29 -0600

    start

---
+++ run.sh
@@ -0,0 +1,2 @@
+#ansible all -i './hosts/inventory' -m ping --private-key='~/.ssh/ansiblemaster' -u ec2-user -vvvv
+ansible-playbook -i './hosts/inventory' --private-key='~/.ssh/ansiblemaster' --connection=local ./playbooks/helloworld.yml
